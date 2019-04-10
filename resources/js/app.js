d124ad7e964f061ece960536e30b0a8e05876554
eFrane <efrane@meanderingsoul.com>
2019-04-10 11:26:03 +0200

    Re-enable the default demo mode

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
 import Bookstore from './components/Bookstore'
 import { createVueInstance } from './init/createVueInstance'
 
+// noinspection JSUnusedGlobalSymbols
 createVueInstance({
   Bookstore
 }, function () {
-  this.$store.api.book.list({ include: 'author,genres' }).then((data) => {
+  this.$store.api.book.list({ include: 'author,genres' }).then(({ data }) => {
     this.$store.dispatch('book/setList', data.book)
     this.$store.dispatch('author/setList', data.author)
     this.$store.dispatch('genre/setList', data.genre)
