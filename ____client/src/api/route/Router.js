f4ee3f30b2458c538a965156a06e539be9f92977
eFrane <efrane@meanderingsoul.com>
2019-04-16 13:11:46 +0200

    A little bit of router documentation

--- ____client/src/api/route/Router.js
+++ ____client/src/api/route/Router.js
@@ -1,3 +1,19 @@
+/**
+ * Basic router implementation for the ResourcefulApi.
+ *
+ * Automagically creating api bound modules builds on
+ * an understanding of the available routes. To
+ * easily instantiate a Store bound to an endpoint,
+ * route information for that endpoint must be provided.
+ *
+ * Since every endpoint is implemented differently and
+ * the choice where this route information comes from
+ * should be left to the endpoint developer, this
+ * library only assumes that route loading is usually an
+ * asynchronous process which eventually returns and
+ * has a set of keyed-by-name `Route` objects in
+ * `this.routes`.
+ */
 export class Router {
   constructor () {
     this.routes = {}
