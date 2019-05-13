f954d036981b5beaf72f569cd97a7f34be483053
Stefan Graupner <efrane@meanderingsoul.com>
2019-05-13 17:40:10 +0200

    Prepare for npm packaging

---
+++ src/lib.js
@@ -0,0 +1,12 @@
+import { createVuexStore } from './init/createVuexStore'
+
+import { Api } from './api/Api'
+import { StaticRouter } from './api/route/StaticRouter'
+import { SimpleRouter } from './api/route/SimpleRouter'
+
+export {
+  createVuexStore,
+  Api,
+  StaticRouter,
+  SimpleRouter
+}
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const merge = require('webpack-merge')
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+let common = {
+  externals: [
+    'vue'
+  ],
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: file => (
+          /node_modules/.test(file) &&
+          !/\.vue\.js/.test(file)
+        )
+      },
+    ]
+  },
+  output: {
+    path: resolve('/dist')
+  }
+}
+
+module.exports = [
+  // browser build
+  merge(common, {
+    mode: 'production',
+    entry: resolve('/src/lib.js'),
+    output: {
+      filename: 'browser/vuex-json-api.min.js',
+    },
+  }),
+
+  // node module build
+  merge(common, {
+    mode: 'production',
+    entry: resolve('/src/lib.js'),
+    output: {
+      filename: 'vuex-json-api.umd.js',
+      libraryTarget: 'umd',
+
+      library: 'vuex-json-api',
+      umdNamedDefine: true
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: './src/**/*',
+          to: '',
+          ignore: ['App.vue', 'main.js']
+        }
+      ]),
+    ]
+  })
+];
