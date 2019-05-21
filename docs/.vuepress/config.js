fbc0af6154bbf8791f107d1aa6110a3c816d8af1
eFrane <efrane@meanderingsoul.com>
2019-05-21 13:52:38 +0200

    Bootstrap vuepress documentation

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: '@efrane/vuex-json-api',
+  description: 'A Json:Api integration for Vuex',
+  themeConfig: {
+    nav: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/eFrane/vuex-json-api'
+      }
+    ],
+    sidebar: 'auto'
+  }
+}
