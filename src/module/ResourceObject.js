b16d5b491e039a871c39b409d718057d96332e47
eFrane <efrane@meanderingsoul.com>
2019-05-29 13:26:27 +0200

    Work on relationship resolution

--- src/module/ResourceObject.js
+++ src/module/ResourceObject.js
@@ -23,7 +23,18 @@ export class ResourceObject {
   addRelationshipResolvers (resultObjectRelationships) {
     for (const relatedObjectType in resultObjectRelationships) {
       if (resultObjectRelationships.hasOwnProperty(relatedObjectType)) {
-        resultObjectRelationships[relatedObjectType].get = this.resolveRelationshipForObjectType(relatedObjectType)
+        let isToManyRelationship = resultObjectRelationships[relatedObjectType].constructor === Array
+
+        resultObjectRelationships[relatedObjectType].get = this.resolveToOneRelationship(
+          resultObjectRelationships[relatedObjectType],
+          isToManyRelationship
+        )
+
+        if (isToManyRelationship) {
+          resultObjectRelationships[relatedObjectType].list = this.resolveToManyRelationship(
+            resultObjectRelationships[relatedObjectType]
+          )
+        }
       }
     }
 
@@ -32,27 +43,40 @@ export class ResourceObject {
 
   /**
    *
-   * @param {String} relatedObjectType
+   * @param {Object} relatedObject
    */
-  resolveRelationshipForObjectType (relatedObjectType) {
+  resolveToOneRelationship (relatedObject, isToManyRelationship) {
     return new Proxy(() => {}, {
       apply (target, thisArg, argArray) {
-        console.dir(arguments)
-        /*
-          thisArg is the relationship obj with `data` and `get()` function;
-          should return the objects from the related modules
+        let moduleName = relatedObject.type.toLower()
+        let relatedModule = store[moduleName]
 
-          what if they are not loaded?
+        if (isToManyRelationship) {
+          let [requestedId] = argArray
 
-          options:
+          try {
+            return relatedModule.items.filter(item => {
+              return item.id === requestedId
+            })[0]
+          } catch (e) {
+            throw new Error(`Related object ${relatedObject.id} not found in ${moduleName}`)
+          }
+        }
 
-          a) throw an error indicating the missing objects
-          b) turn this into an async function which only returns once the items are loaded
-        */
+        return relatedModule.item
       }
     })
+  }
 
-    // TODO: if relationship is a toMany, it should also have an all() method or something like that
-    // TODO: to get the collection of all related items
+  /**
+   *
+   * @param {Array} relatedObjects
+   */
+  resolveToManyRelationship (relatedObjects) {
+    return new Proxy(() => {}, {
+      apply (target, thisArg, argArray) {
+        console.log(arguments)
+      }
+    })
   }
 }
