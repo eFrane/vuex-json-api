d88deaffc5b3107591fe8b2b6f27dd78e47ab54f
Stefan Graupner <efrane@meanderingsoul.com>
2019-06-11 16:52:36 +0200

    Make clean data copies in ResourceObject

--- src/module/ResourceObject.js
+++ src/module/ResourceObject.js
@@ -5,7 +5,7 @@ export class ResourceObject {
    * @param {object} resultObject
    */
   constructor (store, resultObject) {
-    let obj = resultObject.data
+    let obj = JSON.parse(JSON.stringify(resultObject.data))
 
     if (obj.hasOwnProperty('relationships')) {
       obj.relationships = this.addRelationshipResolvers(store, obj.relationships)
