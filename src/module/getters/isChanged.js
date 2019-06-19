5ab9f1d0a4ef19f643fa795565e2399106f0430c
eFrane <efrane@meanderingsoul.com>
2019-06-19 08:45:45 +0200

    Stub a isChanged getter

---
+++ src/module/getters/isChanged.js
@@ -0,0 +1,16 @@
+import {diff} from 'deep-object-diff'
+
+/**
+ * Determine if an object has been changed
+ * @param {Object} state
+ */
+export function hasChanges (isCollection) {
+  return new Proxy(() => { }, {
+    apply (target, thisArg, [ state ]) {
+      let currentState = (isCollection) ? state.items : state.item
+      let initialState = this.initial
+
+      return diff(initialState, currentState) === {}
+    }
+  })
+}
