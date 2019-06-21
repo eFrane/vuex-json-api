c54e21cd25f251fcfdfb1d11ea00ee8e72a2e7c4
eFrane <efrane@meanderingsoul.com>
2019-06-21 10:19:47 +0200

    Refactor validateResourceObject to explicitly disallow ids in newly created resources

--- src/module/helpers/validateResourceObject.js
+++ src/module/helpers/validateResourceObject.js
@@ -2,21 +2,28 @@
  * Do a crude check for common json:api resource properties.
  * This is (probably) not a long-term solution!
  *
+ * Assumptions made:
+ *
+ * - Resource Objects always have a type and a data section
+ * - newly created resources must not have an id (this is slightly
+ *   stricter than the specification allows for and may be changed
+ *   in the future)
+ * - every other resource object should have an id
+ *
  * @param {Object} resourceObject
+ * @param {Boolean} isCreateResource
  */
-export function validateResourceObject (resourceObject, createResource) {
-  if (createResource &&
-    typeof resourceObject === 'object' &&
+export function validateResourceObject (resourceObject, isCreateResource) {
+  let conformsToBasicStructure = typeof resourceObject === 'object' &&
     resourceObject.hasOwnProperty('type') &&
-    resourceObject.hasOwnProperty('data')) {
-    return true
+    resourceObject.hasOwnProperty('data')
+
+  if (isCreateResource) {
+    return conformsToBasicStructure && !resourceObject.hasOwnProperty('id')
   }
-  if (!createResource &&
-    typeof resourceObject === 'object' &&
-    resourceObject.hasOwnProperty('type') &&
-    resourceObject.hasOwnProperty('id') &&
-    resourceObject.hasOwnProperty('data')) {
-    return true
+
+  if (!isCreateResource) {
+    return conformsToBasicStructure && resourceObject.hasOwnProperty('id')
   }
 
   throw new Error('Detected potentially invalid json:api resource object')
