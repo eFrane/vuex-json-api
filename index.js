11e1410d83334b43ae24518ddc5f0c90973fca27
eFrane <efrane@meanderingsoul.com>
2019-06-22 11:04:11 +0200

    Fix main file imports / exports

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { createVuexStore } from './src/init/createVuexStore'
 import { initJsonApiPlugin } from './src/init/initJsonApiPlugin'
 import { StaticRouter } from './src/route/StaticRouter'
-import { SimpleRouter } from './route/SimpleRouter'
+import { JsonApiRouter } from './src/route/JsonApiRouter'
+import { FosJsRoutingRouter } from './src/route/FosJsRoutingRouter'
 
 export {
   createVuexStore,
   initJsonApiPlugin,
   StaticRouter,
-  SimpleRouter
+  JsonApiRouter,
+  FosJsRoutingRouter
 }
