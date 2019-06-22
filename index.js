83adabc865676cb5495b5c648cb3679f9a30828d
eFrane <efrane@meanderingsoul.com>
2019-06-22 10:58:25 +0200

    Simplify package build

---
+++ index.js
@@ -0,0 +1,11 @@
+import { createVuexStore } from './src/init/createVuexStore'
+import { initJsonApiPlugin } from './src/init/initJsonApiPlugin'
+import { StaticRouter } from './src/route/StaticRouter'
+import { SimpleRouter } from './route/SimpleRouter'
+
+export {
+  createVuexStore,
+  initJsonApiPlugin,
+  StaticRouter,
+  SimpleRouter
+}
