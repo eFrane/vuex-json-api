fcfec42a34df2e582cea7300177e43a734d514cf
eFrane <efrane@meanderingsoul.com>
2019-07-03 13:25:08 +0200

    Make loading related objects work

--- src/module/resource/loadRelationship.js
+++ src/module/resource/loadRelationship.js
@@ -1,12 +1,12 @@
 export function loadRelationship (store, currentObjectId, currentObjectType, relatedObject, config) {
-  return new Proxy(() => { }, {
-    apply (target, thisArg, argArray) {
-      let relatedObjectType = (config.isToMany) ? relatedObject.data[0].type : relatedObject.data.type
-      let relatedObjectNameForAction = relatedObjectType.getChar(0).toUpperCase() + relatedObjectType.slice(1)
+  return () => {
+    let relatedObjectType = (config.isToMany) ? relatedObject.data[0].type : relatedObject.data.type
+    let relatedObjectNameForAction = relatedObjectType[0].toUpperCase() + relatedObjectType.slice(1)
 
-      if (config.isToMany) {
-        return store.dispatch(`${currentObjectType}/listRelated${relatedObjectNameForAction}`, { currentObjectId })
-      }
+    if (config.isToMany) {
+      return store.dispatch(`${currentObjectType}/listRelated${relatedObjectNameForAction}`, { id: currentObjectId })
     }
-  })
+
+    return new Error('Failed to load relationship')
+  }
 }
