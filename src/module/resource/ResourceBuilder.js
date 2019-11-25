1ecfd9064cf7810b444ace71db931a8b7971b4c2
florian salis <salis@demos-deutschland.de>
2019-11-25 22:05:08 +0100

    refs #65: restore relationsship access
    - the shorthand should be optional

--- src/module/resource/ResourceBuilder.js
+++ src/module/resource/ResourceBuilder.js
@@ -35,11 +35,45 @@ export class ResourceBuilder {
     return obj
   }
 
+  /**
+   * Adds Methods (get, load, list) to the relationships for getting / loading them
+   * Adds Shorthand Methods (rel / loadRel)
+   *
+   * @param {Object} jsonResourceObject
+   */
   buildRelationshipMethods (obj) {
     const relationships = obj.relationships
 
-    obj.loadRel = (relationshipName) => loadRelationship(this.store, obj.id, obj.type, relationships[relationshipName], getRelationshipConfig(relationships[relationshipName]))()
-    obj.rel = (relationshipName) => getRelationshipConfig(relationships[relationshipName]).isToMany ? listRelationship(this.store, relationships[relationshipName])() : getRelationship(this.store, relationships[relationshipName], getRelationshipConfig(relationships[relationshipName]))()
+    for (const currentRelationshipName in relationships) {
+      if (Object.prototype.hasOwnProperty.call(relationships, currentRelationshipName)) {
+        const relatedObject = relationships[currentRelationshipName]
+
+        const relationshipConfig = {
+          isToMany: relatedObject.data.constructor === Array
+        }
+
+        relatedObject.get = getRelationship(this.store, relatedObject, relationshipConfig)
+        relatedObject.load = loadRelationship(this.store, obj.id, obj.type, relatedObject, relationshipConfig)
+
+        if (relationshipConfig.isToMany) {
+          relatedObject.list = listRelationship(this.store, relatedObject)
+        }
+
+        relationships[currentRelationshipName] = relatedObject
+      }
+    }
+
+    // shorthand variant
+    obj.loadRel = (relationshipName) => {
+      return loadRelationship(this.store, obj.id, obj.type, relationships[relationshipName], getRelationshipConfig(relationships[relationshipName]))()
+    }
+    obj.rel = (relationshipName) => {
+      if (getRelationshipConfig(relationships[relationshipName]).isToMany) {
+        return listRelationship(this.store, relationships[relationshipName])()
+      } else {
+        return getRelationship(this.store, relationships[relationshipName], getRelationshipConfig(relationships[relationshipName]))()
+      }
+    }
   }
 
   /**
