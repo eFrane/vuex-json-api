b5c2c9ce69d564efad3b793072470120b01923a9
Heidrun Wiemold <wiemold@demos-deutschland.de>
2020-02-10 14:46:23 +0100

    make presetModuleName available via ModuleBuilder to be able to access the presetModule instead of the (parent)module

--- src/module/actions/restoreFromInitialAction.js
+++ src/module/actions/restoreFromInitialAction.js
@@ -1,14 +1,24 @@
-export function restoreFromInitialAction (moduleName, isCollection) {
+export function restoreFromInitialAction (moduleName, presetModuleName, isCollection) {
   return new Proxy(() => { }, {
     apply (target, thisArg, [vuexFns, id]) {
       let item
 
-      if (isCollection) {
-        item = thisArg.state[moduleName].initial[id]
-      }
+      if (presetModuleName === null) {
+        if (isCollection) {
+          item = thisArg.state[moduleName].initial[id]
+        }
 
-      if (!isCollection) {
-        item = thisArg.state[moduleName].initial
+        if (!isCollection) {
+          item = thisArg.state[moduleName].initial
+        }
+      } else {
+        if (isCollection) {
+          item = thisArg.state[moduleName][presetModuleName].initial[id]
+        }
+
+        if (!isCollection) {
+          item = thisArg.state[moduleName][presetModuleName].initial
+        }
       }
 
       vuexFns.commit('set', { id: id, data: item })
