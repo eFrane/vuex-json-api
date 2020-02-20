82051cf17321c2f158fa1472b8aa8584541fe51a
florian salis <salis@demos-deutschland.de>
2020-02-20 11:48:16 +0100

    refs #80: switch from if to switch
    - for better readability espacially when the amount of options grows

--- src/module/actions/options/saveOptions.js
+++ src/module/actions/options/saveOptions.js
@@ -1,18 +1,20 @@
+/* eslint-disable no-fallthrough */
 export function saveOptions ({ currentItemState, changedItemState, options }) {
   const returnedItemState = JSON.parse(JSON.stringify(changedItemState))
 
   if (typeof options !== 'undefined' && Object.prototype.hasOwnProperty.call(returnedItemState, 'attributes')) {
-    if (Object.prototype.hasOwnProperty.call(options, 'sendFullAttributes')) {
-      options.sendFullAttributes.forEach(attr => {
-        if (Object.prototype.hasOwnProperty.call(returnedItemState.attributes, attr)) {
+    switch (Object.values(options)) {
+      case 'sendFullAttributes':
+        options.sendFullAttributes.forEach(attr => {
+          if (Object.prototype.hasOwnProperty.call(returnedItemState.attributes, attr)) {
+            returnedItemState.attributes[attr] = currentItemState.attributes[attr]
+          }
+        })
+
+      case 'sendUnchangedAttributes':
+        options.sendUnchangedAttributes.forEach(attr => {
           returnedItemState.attributes[attr] = currentItemState.attributes[attr]
-        }
-      })
-    }
-    if (Object.prototype.hasOwnProperty.call(options, 'sendUnchangedAttributes')) {
-      options.sendUnchangedAttributes.forEach(attr => {
-        returnedItemState.attributes[attr] = currentItemState.attributes[attr]
-      })
+        })
     }
   }
 
