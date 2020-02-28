ea3c0178ddf24efc96b2663bd01dc795ba608473
florian salis <salis@demos-deutschland.de>
2020-02-28 14:22:22 +0100

    refs #80: move attributes-options to own method // fix them
    - use deepMerge to apply the changed values tu the original object/array
    - for 'unchanged':
    -- send unchanged attribute from original data only if its unchanged
    -- otherwise use the changed ones
    - for 'full':
    -- deepmerge object/array to ensure the original object is send
    -- with the changed data

--- src/module/actions/options/saveOptions.js
+++ src/module/actions/options/saveOptions.js
@@ -2,22 +2,49 @@ import { deepMerge } from '@/helpers/deepMerge'
 
 export function saveOptions ({ currentItemState, changedItemState, options }) {
   const returnedItemState = JSON.parse(JSON.stringify(changedItemState))
-
   if (typeof options !== 'undefined' && Object.prototype.hasOwnProperty.call(returnedItemState, 'attributes')) {
-    switch (Object.values(options)) {
-      case 'sendFullAttributes':
-        options.sendFullAttributes.forEach(attr => {
-          if (Object.prototype.hasOwnProperty.call(returnedItemState.attributes, attr)) {
-            returnedItemState.attributes[attr] = currentItemState.attributes[attr]
-          }
-        })
-
-      case 'sendUnchangedAttributes':
-        options.sendUnchangedAttributes.forEach(attr => {
-          returnedItemState.attributes[attr] = currentItemState.attributes[attr]
-        })
-    }
+    Object.keys(options).forEach(option => {
+      switch (option) {
+        case 'attributes':
+          returnedItemState.attributes = attributeOptions({ currentItemState, changedAttributes: returnedItemState.attributes, options: options[option] })
+      }
+    })
   }
 
   return returnedItemState
+}
+
+const attributeOptions = ({ currentItemState, changedAttributes, options }) => {
+  Object.keys(options).forEach(option => {
+    switch (option) {
+      case 'full':
+        if (typeof options.full === 'string') {
+          if (Object.prototype.hasOwnProperty.call(changedAttributes, options.full)) {
+            changedAttributes[options.full] = deepMerge(currentItemState.attributes[options.full], changedAttributes[options.full])
+          }
+        } else {
+          options.full.forEach(attr => {
+            if (Object.prototype.hasOwnProperty.call(changedAttributes, attr)) {
+              changedAttributes[options.full] = deepMerge(currentItemState.attributes[attr], changedAttributes[attr])
+            }
+          })
+        }
+        break
+      case 'unchanged':
+        if (typeof options.unchanged === 'string') {
+          if (Object.prototype.hasOwnProperty.call(changedAttributes, options.unchanged)) {
+            changedAttributes[options.unchanged] = changedAttributes[options.unchanged] ? changedAttributes[options.unchanged] : currentItemState.attributes[options.unchanged]
+          }
+        } else {
+          options.unchanged.forEach(attr => {
+            if (Object.prototype.hasOwnProperty.call(changedAttributes, attr)) {
+              changedAttributes[attr] = changedAttributes[attr] ? changedAttributes[attr] : currentItemState.attributes[attr]
+            }
+          })
+        }
+        break
+    }
+  })
+
+  return changedAttributes
 }
