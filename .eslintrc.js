4f20b02754f7b42661c1fe99f6796fa2f71a8677
Stefan Graupner <efrane@meanderingsoul.com>
2021-04-15 00:06:56 +0200

    feat: Allow disabled/skipped tests
    It is from time to time necessary to skip tests
    while still having the linting succeed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'jest/no-disabled-tests': 'off'
   }
 }
