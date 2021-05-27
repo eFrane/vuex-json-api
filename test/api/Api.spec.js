b1b854b891874f4b2a43127058ccdfd1191f1a51
Stefan Graupner <efrane@meanderingsoul.com>
2021-05-28 00:36:15 +0200

    chore: Remove skipped test
    This part of the lib will be rewritten in this branch,
    there is no use in keeping this then definitely broken
    test around.

--- test/api/Api.spec.js
+++ test/api/Api.spec.js
@@ -18,23 +18,3 @@ test('allows changing the base url', () => {
 
   expect(unconfiguredApi.baseUrl).toBe('/api/')
 })
-
-test.skip('sends requests to the server', () => {
-  return api.get('tag/1').then(
-    data => {
-      expect(data).toBeDefined()
-      expect(data.data).toStrictEqual({
-        data: {
-          attributes: {
-            title: 'Tag 1'
-          },
-          id: '1',
-          type: 'tag'
-        },
-        jsonapi: {
-          version: '1.0'
-        }
-      })
-    }
-  )
-})
