c3c08208a02f47d3a74825581876b2cff64776d5
Stefan Graupner <efrane@meanderingsoul.com>
2021-05-27 22:03:11 +0200

    feat: Add test for hasOwn()

--- test/shared/utils.spec.js
+++ test/shared/utils.spec.js
@@ -1,4 +1,4 @@
-import { deref } from '@/shared/utils'
+import { deref, hasOwn } from '@/shared/utils'
 
 test('dereferences objects', () => {
   const testObj = { foo: 'bar' }
@@ -6,4 +6,16 @@ test('dereferences objects', () => {
 
   expect(dereferenced).not.toBe(testObj)
   expect(dereferenced).toEqual(testObj)
+})
+
+test('has own checks for own properties', () => {
+  const foo = { foo: 'foo' }
+
+  const bar = Object.create(foo)
+  bar.baz = 'baz'
+
+  expect(hasOwn(foo, 'foo')).toBeTruthy()
+  expect(hasOwn(bar, 'foo')).toBeFalsy()
+  expect(hasOwn(foo, 'bar')).toBeFalsy()
+  expect(hasOwn(bar, 'baz')).toBeTruthy()
 })
