d30da0746bb13c8d4d5cb18442fe331a4f04624d
Stefan Graupner <efrane@meanderingsoul.com>
2021-05-27 22:19:41 +0200

    feat: Add test for isAbsoluteUri

--- test/shared/utils.spec.js
+++ test/shared/utils.spec.js
@@ -1,4 +1,4 @@
-import { deref, hasOwn } from '@/shared/utils'
+import { deref, hasOwn, isAbsoluteUri } from '@/shared/utils'
 
 test('dereferences objects', () => {
   const testObj = { foo: 'bar' }
@@ -18,4 +18,21 @@ test('has own checks for own properties', () => {
   expect(hasOwn(bar, 'foo')).toBeFalsy()
   expect(hasOwn(foo, 'bar')).toBeFalsy()
   expect(hasOwn(bar, 'baz')).toBeTruthy()
+})
+
+test('tells relative and absolute uris apart', () => {
+  /**
+   * List of `uri, is_absolute` pairs for testing.
+   *
+   * @type {(string|boolean)[][]}
+   */
+  const testData = [
+    ['/my/path', false],
+    ['//abso.lute/ly', true],
+    ['https://abso.lute/ly', true]
+  ]
+
+  for (const [uri, isAbsolute] of testData) {
+    expect(isAbsoluteUri(uri)).toStrictEqual(isAbsolute)
+  }
 })
