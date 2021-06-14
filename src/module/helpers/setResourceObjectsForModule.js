6db9e80a08018256a4224ccfb7967a0454ac1334
Stefan Graupner <efrane@meanderingsoul.com>
2021-06-14 11:06:24 +0200

    feat: first attempt at better collection handling (#237)
    * feat: first attempt at better collection handling
    
    This tries to set all objects at once when a collection list is
    requested. Along the way, a few minor improvements were made.
    
    * fix: repair resource building
    
    * fix: resource builder only receives normalized input
    
    * fix: merge items instead of overwriting
    
    On setAll, new items must be merged with existing
    ones instead of overwriting the contents of the
    module.
    
    * style: Use correct type hint

--- src/module/helpers/setResourceObjectsForModule.js
+++ src/module/helpers/setResourceObjectsForModule.js
@@ -1,4 +1,22 @@
-import { hasOwn } from '../../shared/utils'
+/**
+ * Make sure we call the correct mutation depending on whether we're mutating
+ * our own state or that of an included module.
+ *
+ * @param {string} currentModule
+ * @param {string} destinationModule
+ * @returns {string}
+ */
+function compileMutation (currentModule, destinationModule) {
+  const isRootMutation = currentModule !== destinationModule
+
+  let mutation = 'set'
+
+  if (isRootMutation) {
+    mutation = destinationModule + '/' + mutation
+  }
+
+  return { mutation, isRootMutation }
+}
 
 /**
  *
@@ -8,18 +26,7 @@ import { hasOwn } from '../../shared/utils'
  * @param {*} objects
  */
 export function setResourceObjectsForModule (vuexFns, currentModule, destinationModule, objects) {
-  for (const id in objects) {
-    if (hasOwn(objects, id)) {
-      const isRootMutation = currentModule !== destinationModule
-      let mutation = 'set'
+  const { mutation, isRootMutation } = compileMutation(currentModule, destinationModule)
 
-      if (isRootMutation) {
-        mutation = destinationModule + '/' + mutation
-      }
-
-      const payload = { id: objects[id].id, data: objects[id] }
-
-      vuexFns.commit(mutation, payload, { root: isRootMutation })
-    }
-  }
+  vuexFns.commit(mutation, objects, { root: isRootMutation })
 }
