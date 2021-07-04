686d1a4152615ecd59bdc55beabb17ef278f3389
Stefan Graupner <efrane@meanderingsoul.com>
2021-07-04 18:59:00 +0200

    refactor: Inline validateResourceObject check

--- src/module/actions/createAction.js
+++ src/module/actions/createAction.js
@@ -1,5 +1,38 @@
 import { processResponseData } from '../helpers/processResponseData'
-import { validateResourceObject } from '../helpers/validateResourceObject'
+import { hasOwn } from '../../shared/utils'
+
+/**
+ * Do a crude check for common json:api resource properties.
+ * This is (probably) not a long-term solution!
+ *
+ * Assumptions made:
+ *
+ * - Resource Objects always have a type and a data section
+ * - newly created resources must not have an id (this is slightly
+ *   stricter than the specification allows for and may be changed
+ *   in the future)
+ * - every other resource object should have an id
+ *
+ * @param {Object} resourceObject
+ * @param {Boolean} hasId
+ */
+export function validateResourceObject (resourceObject, hasId) {
+  const conformsToBasicStructure = typeof resourceObject === 'object' &&
+    hasOwn(resourceObject, 'type') &&
+    hasOwn(resourceObject, 'data')
+
+  if (hasId) {
+    return conformsToBasicStructure &&
+      !hasOwn(resourceObject, 'id')
+  }
+
+  if (!hasId) {
+    return conformsToBasicStructure &&
+      hasOwn(resourceObject, 'id')
+  }
+
+  throw new Error('Detected potentially invalid json:api resource object')
+}
 
 /**
  * Create a new resource
