ebeeecbdbca6545d56a2f36490c46e38392c093f
Stefan Graupner <efrane@meanderingsoul.com>
2021-07-04 19:02:17 +0200

    refactor: Only check for basic resource structure
    As resource objects with and without client-generated
    ids have to be considered for the createAction it does
    not make sense to do that check during the basic
    structure validation.

--- src/module/actions/createAction.js
+++ src/module/actions/createAction.js
@@ -5,33 +5,13 @@ import { hasOwn } from '../../shared/utils'
  * Do a crude check for common json:api resource properties.
  * This is (probably) not a long-term solution!
  *
- * Assumptions made:
- *
- * - Resource Objects always have a type and a data section
- * - newly created resources must not have an id (this is slightly
- *   stricter than the specification allows for and may be changed
- *   in the future)
- * - every other resource object should have an id
- *
  * @param {Object} resourceObject
- * @param {Boolean} hasId
+ * @returns Boolean
  */
-export function validateResourceObject (resourceObject, hasId) {
-  const conformsToBasicStructure = typeof resourceObject === 'object' &&
+export function validateResourceObject (resourceObject) {
+  return typeof resourceObject === 'object' &&
     hasOwn(resourceObject, 'type') &&
     hasOwn(resourceObject, 'data')
-
-  if (hasId) {
-    return conformsToBasicStructure &&
-      !hasOwn(resourceObject, 'id')
-  }
-
-  if (!hasId) {
-    return conformsToBasicStructure &&
-      hasOwn(resourceObject, 'id')
-  }
-
-  throw new Error('Detected potentially invalid json:api resource object')
 }
 
 /**
@@ -43,7 +23,9 @@ export function validateResourceObject (resourceObject, hasId) {
 export function createAction (api, moduleName) {
   return new Proxy(() => {}, {
     apply (target, thisArg, [vuexFns, resourceObject]) {
-      validateResourceObject(resourceObject, true)
+      if (!validateResourceObject(resourceObject)) {
+        throw new Error('Please pass valid json:api resource objects to this action.')
+      }
 
       vuexFns.commit('startLoading')
 
