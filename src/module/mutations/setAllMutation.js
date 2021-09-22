4649695755c77610928213dc3d697425a915fe6f
Florian Salis <salis@demos-deutschland.de>
2021-09-22 10:44:41 +0200

    fix setAllMutation
    in some cases the method is called with just one
    item as payload.
    In this case we just have to update that item within the collection.

--- src/module/mutations/setAllMutation.js
+++ src/module/mutations/setAllMutation.js
@@ -1,3 +1,6 @@
+import { hasOwn } from '../../shared/utils'
+import Vue from 'vue'
+
 /**
  * Proxy for setting Resource Objects on a collection module
  *
@@ -12,8 +15,13 @@ export function setAllMutation (resourceBuilder) {
     apply (target, thisArg, [state, payload]) {
       const settablePayload = resourceBuilder.build(payload)
 
-      state.items = { ...state.items, ...settablePayload }
-      state.initial = { ...state.initial, ...settablePayload }
+      if (hasOwn(settablePayload, 'id')) {
+        Vue.set(state.items, settablePayload.id, settablePayload)
+        Vue.set(state.initial, settablePayload.id, settablePayload)
+      } else {
+        state.items = {...state.items, ...settablePayload}
+        state.initial = {...state.initial, ...settablePayload}
+      }
     }
   })
 }
