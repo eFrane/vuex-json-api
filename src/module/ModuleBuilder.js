00d1f46f010ac58ffa8a3a0c9196e083117a62f2
salisdemos <40487461+salisdemos@users.noreply.github.com>
2022-05-13 16:08:52 +0200

    fix: pass isCollection to update mutation (#436)

--- src/module/ModuleBuilder.js
+++ src/module/ModuleBuilder.js
@@ -129,7 +129,7 @@ export class ModuleBuilder {
       setItem: setItemMutation(resourceBuilder, this.isCollection),
       startLoading: startLoadingMutation,
       endLoading: endLoadingMutation,
-      update: updateMutation
+      update: updateMutation(this.isCollection)
     }
 
     if (this.apiMethods.allowsDeletion()) {
